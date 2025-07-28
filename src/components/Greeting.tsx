4e5409479ccf2080aa16b120b85f96626dfa9a89
arinjay-gangwar <arinjay.gangwar@digitup.in>
2025-07-28 10:40:23 +0530

    Add Greeting component

---
+++ src/components/Greeting.tsx
@@ -0,0 +1,10 @@
+function Greeting() {
+  return (
+    <div>
+      <h2>Welcome to React Learning</h2>
+      <p>Today is a great day to learn something new 🚀</p>
+    </div>
+  );
+}
+
+export default Greeting;
